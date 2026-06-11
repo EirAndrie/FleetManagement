b087fcfa5376c86621ae32ef2ae967be020eff03
drie <hrieu01@gmail.com>
2026-06-11 23:30:49 +0800

    Made the vehicle type to default modern

--- backend/src/db/schemas/Vehicle.ts
+++ backend/src/db/schemas/Vehicle.ts
@@ -3,14 +3,16 @@ import { Cooperatives } from './Cooperative';
 
 export const Vehicles = pgTable("vehicles", {
     vehicleId: uuid("vehicle_id").primaryKey().defaultRandom(),
+
     plateNumber: text("license_plate").notNull().unique(),
     unitNumber: text("unit_number").notNull().unique(),
     make: text("make").notNull(),
     model: text("model").notNull(),
     year: text("year").notNull(),
     capacity: text("capacity").notNull(),
-    type: text("type").notNull(), // Modern or Traditional
-    status: text("status").notNull(),
+    type: text("type").notNull().default("Modern"), // Modern or Traditional
+    status: text("status").notNull().default("Active"),
+
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 
